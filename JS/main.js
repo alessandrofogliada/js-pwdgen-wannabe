c03e49927b8d5fab53926b83ad0ecd9406460991
alessandrofogliada <fogliada.a@gmail.com>
2024-06-03 15:04:59 +0200

    prima prova del calcolatore di password

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente il suo nome 
+let nomeUtente = prompt("inserisci il tuo nome");
+
+// poi chiedi il suo cognome
+let cognomeUtente = prompt("inserisci il tuo cognome");
+
+// poi chiedi il suo colore preferito
+let colorePreferito = prompt("inserisci il tuo colore preferito");
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito23
+document.getElementById("mio_id").innerHTML = nomeUtente + cognomeUtente + colorePreferito + 2023
